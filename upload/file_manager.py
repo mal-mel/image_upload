f5e07f04e3eab579f24bfb2cf235d36e240ae63e
Oleg Svetovidov <olegsvetovidov@gmail.com>
2019-04-15 02:24:36 +0300

    Update file_manager.py

--- upload/file_manager.py
+++ upload/file_manager.py
@@ -9,18 +9,20 @@ def file_manager(request):
     date = str(datetime.today()).replace(' ', '_')[:-7]
 
     for format_img in TYPES:
-
+        print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+        print(os.getcwd())
         if format_img in request.POST['url']:
 
-            os.system(f"wget -O /home/mal-mel/Desktop/WEB/IMAGE_UPLOAD/image_upload/media/img/{date} {request.POST['url']}")
+            os.system(f"wget -O {os.getcwd()}/media/img/{date} {request.POST['url']}")
 
             type_of_file = \
-            os.popen(f'file /home/mal-mel/Desktop/WEB/IMAGE_UPLOAD/image_upload/media/img/{date}').read().split(' ')[1].lower()
+            os.popen(f'file {os.getcwd()}/media/img/{date}').read().split(' ')[1].lower()
 
             os.system(
-                f'mv /home/mal-mel/Desktop/WEB/IMAGE_UPLOAD/image_upload/media/img/{date}'
-                f' /home/mal-mel/Desktop/WEB/IMAGE_UPLOAD/image_upload/media/img/{date}.{type_of_file}')
+                f'mv {os.getcwd()}/media/img/{date}'
+                f' {os.getcwd()}/media/img/{date}.{type_of_file}')
 
             return date, type_of_file
 
     return False
+
