895254fbebb2546d1ad9e46e5ba9ddbd784e4ed0
mal-mel <olegsvetovidov@gmail.com>
2019-06-08 19:43:52 +0300

    )

--- image_upload/settings.py
+++ image_upload/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
