85fc68625d748f5f4bde2c4caf6d0cf854475f71
mal-mel <olegsvetovidov@gmail.com>
2019-06-08 19:46:32 +0300

    ::))

--- image_upload/urls.py
+++ image_upload/urls.py
@@ -14,6 +14,5 @@ urlpatterns = [
     path('img/<str:img>/', get_image)
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
